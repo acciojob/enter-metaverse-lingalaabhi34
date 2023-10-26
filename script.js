1664c93ba2ffcc8dacdbcd6cb3204af3d6c95d40
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-26 23:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const p = document.getElementById("status");
+const button = document.getElementById("enterBtn");
+button.addEventListener("click",onclick);
+function onclick(){
+    const h = document.createElement("h1");
+    p.innerText="";
+    h.innerText="Entered Metaverse";
+    p.appendChild(h);
+}
